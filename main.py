dc9f25edab1d3e5fc9104579103824e375c68058
Screenbones <whoami@kotsu.red>
2024-03-27 00:22:25 -0700

    Day 26: Adding Multiline Text to PDF

--- main.py
+++ main.py
@@ -9,6 +9,11 @@ for filepath in filepaths:
     filename = Path(filepath).stem.capitalize()
     pdf.add_page()
     pdf.set_font(family="Times", size=16, style="B")
-    pdf.cell(w=50, h=8, txt=f"{filename}")
+    pdf.cell(w=50, h=8, txt=f"{filename}", ln=1)
+
+    with open(filepath, "r") as file:
+        content = file.read()
+    pdf.set_font(family="Times", size=12)
+    pdf.multi_cell(w=0, h=6, txt=content)
 
 pdf.output("output.pdf")
